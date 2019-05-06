e66f4111718baf6f50b2aa4b9abbaa3cd1a97704
kumarajyu <ec2-user@ip-172-31-40-213.us-east-2.compute.internal>
2019-05-06 01:56:03 +0000

    6 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
   root 'home#top'
   get 'top', to: 'home#top'
   get 'about', to: 'home#about'
@@ -8,5 +10,7 @@ Rails.application.routes.draw do
   get 'posts/:id', to: 'posts#show', as: :post
   get 'posts/:id/edit', to: 'posts#edit', as: :edit
   post 'posts/:id/update', to: 'posts#update', as: :update_post
-  post 'posts/:id/destroy', to: 'posts#destroy', as: :destroy_post 
+  post 'posts/:id/destroy', to: 'posts#destroy', as: :destroy_post
+  
+  resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190501010943) do
+ActiveRecord::Schema.define(version: 20190504122715) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
