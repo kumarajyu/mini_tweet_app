d8eb0b4de10483774a6b79d191c14ad43c2d3ba4
kumarajyu <kumarajyu0@gmail.com>
2019-05-07 10:13:59 +0000

    9 complete
    x

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,17 @@
 class PostsController < ApplicationController
   before_action :authenticate_user
+  before_action :limitation_correct_user, only: [:edit, :update, :destroy]
   
   def new
     @post = Post.new
   end
   
   def create
-    @post = Post.new(content: params[:content])
+    @post = Post.new(
+      content: params[:content],
+      user_id: @current_user.id,
+      )
+    
     if @post.save
       flash[:notice] = "投稿しました！"
       redirect_to posts_index_url
@@ -21,14 +26,13 @@ class PostsController < ApplicationController
   
   def show
     @post = Post.find(params[:id])
+    @user = @post.user
   end
   
   def edit
-    @post = Post.find(params[:id])
   end
   
   def update
-    @post = Post.find(params[:id])
     @post.content = params[:content]
     if @post.save
       flash[:notice] = "投稿を編集しました。"
@@ -39,9 +43,16 @@ class PostsController < ApplicationController
   end
   
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     flash[:notice] = "投稿を削除しました！"
     redirect_to posts_index_url
+  end
+  
+  def limitation_correct_user
+    @post = Post.find(params[:id])
+    unless @post.user_id == @current_user.id
+      flash[:notice] = "自分以外のユーザーの投稿は編集できません。"
+      redirect_to posts_index_url
+    end
   end
 end
